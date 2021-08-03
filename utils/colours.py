d860948c4ae564e941a9fa188af565225b6bf5c9
IamEinstein <63300310+IamEinstein@users.noreply.github.com>
2021-08-03 14:35:19 +0530

    Remove bug

--- utils/colours.py
+++ utils/colours.py
@@ -1,5 +1,5 @@
 from discord import Color
-color_list = [Color.green(), Color.blue(), Color.red(), Color.yellow(), Color.gold(), Color.dark_blue(), Color.dark_gold(
+color_list = [Color.green(), Color.blue(), Color.red(), Color.gold(), Color.dark_blue(), Color.dark_gold(
 ), Color.dark_green(), Color.dark_red(), Color.dark_teal(), Color.dark_grey(), Color.dark_orange(), Color.dark_magenta()]
 
 
